2101d183887da56505e6674a340f57e6468b026c
Konstantin K <kostikum@yandex.ru>
2020-05-29 23:33:05 +0700

    inital commit

---
+++ app/src/main/java/com/kostikum/lifehackstudio/CompaniesActivity.kt
@@ -0,0 +1,12 @@
+package com.kostikum.lifehackstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompaniesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/kostikum/lifehackstudio/CompanyListFragment.kt
@@ -0,0 +1,13 @@
+package com.kostikum.lifehackstudio
+
+import androidx.fragment.app.Fragment
+
+class CompanyListFragment : Fragment() {
+
+
+
+
+
+
+
+}
